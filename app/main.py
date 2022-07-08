0e8f18026c88453d5b6f9593d70e23f99f9d1be6
Andre Castro <87455696+andrecastropaiva@users.noreply.github.com>
2022-07-08 17:47:28 +0100

    Update main.py

--- app/main.py
+++ app/main.py
@@ -4,7 +4,7 @@ from create_starships_collection import create_starships_collection
 from insert_starships import insert_starships_docs
 from update_pilots import update_pilots
 
-client = pymongo.MongoClient('mongodb://docker:mongopw@localhost:55000')  # connecting pymongo to MongoDB
+client = pymongo.MongoClient()  # connecting pymongo to MongoDB
 db = client['star_wars']  # adding database where we want to connect to
 
 characters = db['characters']  # assigning database characters collection to variable for easier usability
